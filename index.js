16d713f1936cd47169d9a6ca00b0475360dbc1b7
joonho4 <wns4090@gmail.com>
2026-03-31 13:23:09 +0900

    [Feature] authService 추가
    - 비밀 번호 검증 기능 추가
    - 토큰 발급 기능 추가
    - 파일 정리

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import express from "express";
 
-import userRoutes from "./routes/userRoutes.js";
+import userRoutes from "./routes/user/userRoutes.js";
 import errorHandler from "./middleware/errorHandler.js";
 
 const app = express();
