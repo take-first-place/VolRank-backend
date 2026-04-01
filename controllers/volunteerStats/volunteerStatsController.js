2d50b4f9cf3bc56d115efb218c116142ae15228f
kwonsj0825 <kwonsj0825@naver.com>
2026-04-01 15:10:04 +0900

    [Fix] 오타 수정

--- controllers/volunteerStats/volunteerStatsController.js
+++ controllers/volunteerStats/volunteerStatsController.js
@@ -6,7 +6,7 @@ import { success, fail } from "../../utils/response.js";
 
 export const getVolunteerStats = async (req, res) => {
     try {
-        const useId = req.user.id;
+        const userId = req.user.id;
         const stats = await getVolunteerStatsByUserId(userId);
 
         return success(res, stats, "조회 성공", 200);
