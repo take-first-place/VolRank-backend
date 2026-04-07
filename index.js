224293889492db402457d8bd903b68967feabee6
partmant <ghdckdgml191@gmail.com>
2026-04-07 10:18:22 +0900

    [Feat] 봉사활동 동기화 테스트용 컨트롤러 및 라우터 추가

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ import authRoutes from "./routes/auth/authRoutes.js";
 import regionRoutes from "./routes/region/regionRoutes.js";
 import volunteerStatsRoutes from "./routes/volunteerStats/volunteerStatsRoutes.js";
 import rankRoutes from "./routes/rank/rankRoutes.js";
+import publicAPIRouter from "./routes/publicAPI/publicAPIRouter.js";
 
 import certificateRoutes from "./routes/certificate/certificateRoutes.js";
 import errorHandler from "./middleware/errorHandler.js";
@@ -24,7 +25,8 @@ app.use("/api/regions", regionRoutes);
 app.use("/api/volunteer-stats", volunteerStatsRoutes);
 app.use("/uploads", express.static(path.join(process.cwd(), "uploads")));
 app.use("/api/certificates", certificateRoutes);
-app.use("/api/rank", rankRoutes)
+app.use("/api/rank", rankRoutes);
+app.use("/api/volunteers", publicAPIRouter);
 
 // 에러 핸들러 (항상 마지막!)
 app.use(errorHandler);
